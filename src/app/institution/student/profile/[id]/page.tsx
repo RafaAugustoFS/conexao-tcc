b7fe6c7906019efd208eb971d558d994528388fb
Rafael Augusto <rafaelaugustofsilva@gmail.com>
2025-03-26 15:38:12 -0300

    ✨
    Criando toastify para deletar e editar estudante.

--- src/app/institution/student/profile/[id]/page.tsx
+++ src/app/institution/student/profile/[id]/page.tsx
@@ -3,12 +3,14 @@ import Sidebar from "@/components/layout/sidebarInstitution";
 import { ProfileInfo } from "@/components/ui/alunos/profile";
 import { Button } from "@/components/ui/alunos/button";
 import { useEffect, useState } from "react";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 import { Moon, Pencil, Sun, Trash } from "lucide-react";
 import Link from "next/link";
 import DeleteModal from "@/components/modals/modelDelete";
 import { useTheme } from "@/components/ThemeProvider";
-
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 interface StudentProfile {
   imageUrl: string;
@@ -33,13 +35,17 @@ export default function User({
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
-  const [selectedStudentId, setSelectedStudentId] = useState<string | null>(null);
+  const [selectedStudentId, setSelectedStudentId] = useState<string | null>(
+    null
+  );
+  const router = useRouter();
 
   // Função de buscar os dados do estudante
   const fetchStudentData = async () => {
     try {
       const response = await fetch(`http://localhost:3000/api/student/${id}`);
-      if (!response.ok) throw new Error("Não foi possível carregar os dados do estudante");
+      if (!response.ok)
+        throw new Error("Não foi possível carregar os dados do estudante");
 
       const data = await response.json();
       setStudentData(data); // Setando os dados do estudante
@@ -56,7 +62,7 @@ export default function User({
         method: "DELETE",
       });
       if (!response.ok) throw new Error("Erro ao excluir o estudante");
-      
+
       setStudentData(null);
       setIsModalOpen(false);
       setSelectedStudentId(null);
@@ -78,6 +84,10 @@ export default function User({
   const confirmDelete = () => {
     if (selectedStudentId) {
       deleteStudent(selectedStudentId);
+      toast.success("Aluno deletado com sucesso!");
+      setTimeout(() => {
+        router.push("/institution/class");
+      }, 2000);
     }
   };
 
@@ -87,50 +97,61 @@ export default function User({
   }, [darkMode]);
 
   return (
-    <div className="flex min-h-screen bg-[#F0F7FF] dark:bg-[#141414]">
-      <Sidebar />
-      <main className="flex-1">
-        <div className="p-8">
-          <div className="flex items-center justify-end mb-8 w-full">
-            <Button onClick={toggleTheme}>
-              {darkMode ? <Sun size={20} /> : <Moon size={20} />}
-            </Button>
-          </div>
-          <div className="bg-white dark:bg-black rounded-lg shadow-sm p-3">
-            {studentData && (
-              <ProfileInfo
-                imageUrl={studentData.imageUrl || "https://img.freepik.com/free-vector/isolated-young-handsome-man-different-poses-white-background-illustration_632498-855.jpg?t=st=1738800543~exp=1738804143~hmac=5400a6f0c02663ed6f91ff172c490ed49dbd456d03bed9e4c98b2aed06b0dfdb&w=826"}
-                name={studentData.nome}
-                email={studentData.emailAluno}
-                birthDate={studentData.dataNascimentoAluno}
-                phone={studentData.telefoneAluno}
-                registrationNumber={studentData.matriculaAluno}
-              />
-            )}
-            <div className="w-full flex flex-row justify-end space-x-4 pr-8">
-              <Link href={`../../../student/editprofile/${id}`}>
-                <button className="p-2 bg-green-500 text-white rounded-full hover:bg-green-600">
-                  <Pencil size={20} />
+    <>
+      <ToastContainer />
+      <div className="flex min-h-screen bg-[#F0F7FF] dark:bg-[#141414]">
+        <Sidebar />
+        <main className="flex-1">
+          <div className="p-8">
+            <div className="flex items-center justify-end mb-8 w-full">
+              <Button onClick={toggleTheme}>
+                {darkMode ? <Sun size={20} /> : <Moon size={20} />}
+              </Button>
+            </div>
+            <div className="bg-white dark:bg-black rounded-lg shadow-sm p-3">
+              {studentData && (
+                <ProfileInfo
+                  imageUrl={
+                    studentData.imageUrl ||
+                    "https://img.freepik.com/free-vector/isolated-young-handsome-man-different-poses-white-background-illustration_632498-855.jpg?t=st=1738800543~exp=1738804143~hmac=5400a6f0c02663ed6f91ff172c490ed49dbd456d03bed9e4c98b2aed06b0dfdb&w=826"
+                  }
+                  name={studentData.nome}
+                  email={studentData.emailAluno}
+                  birthDate={studentData.dataNascimentoAluno}
+                  phone={studentData.telefoneAluno}
+                  registrationNumber={studentData.matriculaAluno}
+                />
+              )}
+              <div className="w-full flex flex-row justify-end space-x-4 pr-8">
+                <Link href={`../editprofile/${id}`}>
+                  <button className="p-2 bg-green-500 text-white rounded-full hover:bg-green-600">
+                    <Pencil size={20} />
+                  </button>
+                </Link>
+                <button
+                  className="p-2 bg-red-500 text-white rounded-full hover:bg-red-600"
+                  onClick={() => handleDeleteClick(id)}
+                >
+                  <Trash size={20} />
                 </button>
-              </Link>
-              <button
-                className="p-2 bg-red-500 text-white rounded-full hover:bg-red-600"
-                onClick={() => handleDeleteClick(id)}
-              >
-                <Trash size={20} />
-              </button>
-            </div>
-            <div className="flex justify-center w-full">
-            <Link className="text-[#4184ff] hover:underline" href={`./feedback/${id}`}>Ver Mais</Link>
+              </div>
+              <div className="flex justify-center w-full">
+                <Link
+                  className="text-[#4184ff] hover:underline"
+                  href={`./feedback/${id}`}
+                >
+                  Ver Mais
+                </Link>
+              </div>
             </div>
           </div>
-        </div>
-      </main>
-      <DeleteModal
-        isOpen={isModalOpen}
-        onClose={() => setIsModalOpen(false)}
-        onConfirm={confirmDelete} // Só exclui quando confirmar
-      />
-    </div>
+        </main>
+        <DeleteModal
+          isOpen={isModalOpen}
+          onClose={() => setIsModalOpen(false)}
+          onConfirm={confirmDelete} // Só exclui quando confirmar
+        />
+      </div>
+    </>
   );
 }
