0884312ef67a0ba2bdb14f65054f3116a1fe6de0
Mateus Tomé <154273310+MateusTome-Dev@users.noreply.github.com>
2025-03-27 15:47:41 -0300

    🎨

--- src/components/layout/sidebarInstitution.tsx
+++ src/components/layout/sidebarInstitution.tsx
@@ -161,8 +161,8 @@ const SidebarInstitution = () => {
         confirmButtonColor="bg-red-600" // Cor personalizada
         confirmButtonText="Sair"
       >
-        <h2 className="text-lg font-bold mb-4">Confirmar saída</h2>
-        <p>Tem certeza que deseja sair?</p>
+        <h2 className="text-lg font-bold mb-4 dark:text-white">Confirmar saída</h2>
+        <p className="dark:text-white">Tem certeza que deseja sair?</p>
       </Modal>
     </>
   );
