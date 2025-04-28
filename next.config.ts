4880517542d25c619d288e84d7050dde98454bd3
Mateus Tomé <154273310+MateusTome-Dev@users.noreply.github.com>
2025-04-28 15:37:59 -0300

    Revert "Merge pull request #141 from RafaAugustoFS/configuração-deploy-vercel-teste"
    This reverts commit a2ab721c4b9f30c953f331ddbb5551b141306f63, reversing
    changes made to 86d927e9c75d9b1d4e2a556c946aead0b611610c.

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Configuração obrigatória para deploy na Vercel
-  output: 'standalone', // Gera uma pasta standalone otimizada
-
-  // Configuração de otimização de imagens
-  images: {
-    domains: [
+ images: {
+    domains:[
       "img.freepik.com",
       "res.cloudinary.com"
-    ],
-    // Otimizações recomendadas:
-    formats: ['image/webp'], // Habilita conversão para WebP
-    minimumCacheTTL: 60, // Cache mínimo de 60 segundos
-  },
-
-  // Configuração de performance (opcional mas recomendado)
-  compress: true, // Ativa compressão GZIP
-  
+    ]
+  }
 };
 
 export default nextConfig;
--- vercel-build.sh
+++ vercel-build.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
-echo "➡️ Instalando dependências..."
-npm install --force || exit 1
-
-echo "➡️ Rodando build..."
-npm run build || exit 1
-
-echo "➡️ Verificando estrutura de arquivos..."
-ls -R .next  # Lista todos os arquivos gerados
-
-exit 0
+npm install --force
+npm run build
