da408c647cbec68c68b6c6ddd4f53e5da2616872
Mateus Tomé <154273310+MateusTome-Dev@users.noreply.github.com>
2025-04-29 14:19:44 -0300

    🔨

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,9 @@ const eslintConfig = [
     rules: {
       "@next/next/no-html-link-for-pages": "off",       // Exemplo: Desativa aviso de links HTML no Next.js
       "@typescript-eslint/no-unused-vars": "warn",      // Transforma erros de variáveis não usadas em avisos
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
       "no-console": "off",                             // Permite uso de console.log
       "react/no-unescaped-entities": "off",            // Desativa avisos de entidades HTML no JSX
       // Adicione outras regras conforme necessário
